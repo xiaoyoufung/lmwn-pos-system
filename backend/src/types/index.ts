d62ad893c1b2ebefaa5b896db857bfd8de71bfcc
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2026-02-10 03:27:00 +0700

    refactor(backend): update index file

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
-import { autoRegister } from "./infrastructure/di/autoRegister.js";
-import { startHttp } from "./infrastructure/httpServer.js";
+import { autoRegister } from "../infrastructure/di/autoRegister.js";
+import { startHttp } from "../infrastructure/httpServer.js";
 import { container } from "tsyringe";
 
 async function main() {
